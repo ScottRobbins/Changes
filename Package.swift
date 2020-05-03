a1994e9b3859a9a2233bbf0ad303fb773c97a9d2
Scott Robbins <scott.robbins@mobiledefense.com>
2020-05-03 17:43:45 -0400

    Add dependencies

--- Package.swift
+++ Package.swift
@@ -5,16 +5,22 @@ import PackageDescription
 
 let package = Package(
   name: "ChangelogManager",
+  products: [
+    .executable(name: "changelog-manager", targets: ["ChangelogManager"])
+  ],
   dependencies: [
-    // Dependencies declare other packages that this package depends on.
-    // .package(url: /* package url */, from: "1.0.0"),
+    .package(url: "https://github.com/JohnSundell/Files", from: "4.0.0"),
+    .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.0.5")),
+    .package(url: "https://github.com/jpsim/Yams.git", from: "3.0.0"),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages which this package depends on.
     .target(
       name: "ChangelogManager",
-      dependencies: []
+      dependencies: [
+        "Files", .product(name: "ArgumentParser", package: "swift-argument-parser"), "Yams",
+      ]
     ),
     .testTarget(
       name: "ChangelogManagerTests",
