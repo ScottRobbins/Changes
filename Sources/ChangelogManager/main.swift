913205a0d9d7e6cf7b32ff6473c85c6e3dfd4cd2
Scott Robbins <scott.robbins@mobiledefense.com>
2020-05-03 20:11:25 -0400

    Add top-level command

--- Sources/ChangelogManager/main.swift
+++ Sources/ChangelogManager/main.swift
@@ -1,1 +1,12 @@
-print("Hello, world!")
+import ArgumentParser
+
+struct ChangelogManager: ParsableCommand {
+  static var configuration = CommandConfiguration(
+    commandName: nil,
+    abstract: "A utility and framework for managing and generating changelogs",
+    version: "0.0.1",
+    subcommands: []
+  )
+}
+
+ChangelogManager.main()
