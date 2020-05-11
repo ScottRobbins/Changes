ddb987cad2d0baee0b643fe41601b624e1b7f2d4
Scott Robbins <scott.robbins@mobiledefense.com>
2020-05-11 01:44:59 -0400

    Support rerelease and build metadata when adding entries to a release

---
+++ Sources/ChangelogManager/Utils/Version+Strict.swift
@@ -0,0 +1,20 @@
+import Version
+
+extension Version {
+  var release: Version {
+    var mutableSelf = self
+    mutableSelf.prerelease = nil
+    mutableSelf.build = nil
+    return mutableSelf
+  }
+  
+  var isPrerelease: Bool {
+    return self.prerelease != nil
+  }
+  
+  var droppingBuildMetadata: Version {
+    var mutableSelf = self
+    mutableSelf.build = nil
+    return mutableSelf
+  }
+}
