d30a1757d186850e13d0ebf9e159e87519325ab3
Scott Robbins <scott.robbins@mobiledefense.com>
2020-07-11 22:13:26 -0400

    Update version number in code

--- Sources/ChangesCLI/main.swift
+++ Sources/ChangesCLI/main.swift
@@ -4,7 +4,7 @@ struct Changes: ParsableCommand {
   static var configuration = CommandConfiguration(
     commandName: nil,
     abstract: "A utility and framework for managing and generating changelogs",
-    version: "0.0.1",
+    version: "0.1.0",
     subcommands: [Init.self, Add.self, RegenerateChangelog.self, Release.self]
   )
 }
