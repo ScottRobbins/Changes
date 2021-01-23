05f238695abe4dfd4b323a5f73e82c62760342ae
Scott Robbins <scott.robbins@mobiledefense.com>
2021-01-23 16:37:59 -0500

    Fix version requirements

--- Package.swift
+++ Package.swift
@@ -13,10 +13,10 @@ let package = Package(
     .executable(name: "changes", targets: ["ChangesCLI"])
   ],
   dependencies: [
-    .package(url: "https://github.com/JohnSundell/Files", from: "4.0.0"),
+    .package(url: "https://github.com/JohnSundell/Files", .upToNextMajor(from: "4.0.0")),
     .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.3.0")),
-    .package(url: "https://github.com/mrackwitz/Version.git", .exact("0.8.0")),
-    .package(url: "https://github.com/jpsim/Yams.git", from: "4.0.0"),
+    .package(url: "https://github.com/mrackwitz/Version.git", .upToNextMinor(from: "0.8.0")),
+    .package(url: "https://github.com/jpsim/Yams.git", .upToNextMajor(from: "4.0.0")),
   ],
   targets: [
     .target(
